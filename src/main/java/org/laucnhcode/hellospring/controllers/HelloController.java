a1cabdb18cf77910d0ed53b9a52e6feb8e0b9cf2
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-24 21:27:03 -0600

    Created class level controller annotations to simplify code

--- src/main/java/org/laucnhcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/laucnhcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     // Handles request at path /hello
@@ -13,28 +15,26 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
+    // path now /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
+    // path /hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm(){
         return "<html>" +
                     "<body>" +
