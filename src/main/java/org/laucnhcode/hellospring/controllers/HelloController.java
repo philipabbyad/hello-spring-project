7a483bcf6eea635253364ccd71b9cd84c971bbf4
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-27 20:11:08 -0600

    Removed unnessecary commented-out code. Removed unessecary goodbye method.

--- src/main/java/org/laucnhcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/laucnhcode/hellospring/controllers/HelloController.java
@@ -1,36 +1,25 @@
 package org.laucnhcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-    // Handles request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-    @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     @GetMapping("form")
